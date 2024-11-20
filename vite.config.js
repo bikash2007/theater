6d85c07414b5d5776780d44b40575b98fa8958cb
bikash2007 <bikashbhandari600@gmail.com>
2024-11-20 14:53:12 +0545

    testing before hosting

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Replace with your repository name
+  base: "/theater/", // Replace with your repository name
   server: {
     host: "0.0.0.0", // Listens on all network interfaces
     port: 3000, // You can use any available port
