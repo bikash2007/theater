eebda70e89cdb1ef56025d19f7bcb2d65ce88a78
bikash2007 <bikashbhandari600@gmail.com>
2024-12-04 14:30:15 +0545

    sign up

--- src/componets/Auth/Signup.jsx
+++ src/componets/Auth/Signup.jsx
@@ -197,7 +197,7 @@ const Signup = ({ setShowLogin }) => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.3, delay: 0.15 }}
       >
-        Sign Up as Member
+        Sign Up 
       </motion.button>
     </form>
   );
